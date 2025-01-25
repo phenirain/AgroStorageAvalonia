7f67138b86f3b7dd327c9aa7ddb3989e322ea03c
phenirain <phenirain@phenirains-MacBook-Pro.local>
2025-01-26 00:43:59 +0300

    feat: added main page into main window, need to move single page

--- App.axaml.cs
+++ App.axaml.cs
@@ -23,10 +23,15 @@ public partial class App : Application
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
-            desktop.MainWindow = new AuthWindow
+            // desktop.MainWindow = new AuthWindow
+            // {
+            //     DataContext = new AuthWindowViewModel(),
+            // };
+            desktop.MainWindow = new MainWindow("")
             {
-                DataContext = new AuthWindowViewModel(),
+                DataContext = new MainWindowViewModel(),
             };
+            desktop.MainWindow.Show();
         }
 
         base.OnFrameworkInitializationCompleted();
