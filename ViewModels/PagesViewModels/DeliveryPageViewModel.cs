be6b0505d5f613de58e7f300b72e64e291eaba3e
phenirain <bigrus1998@gmail.com>
2025-01-26 15:44:50 +0300

    feat: added delivery page without logic

--- ViewModels/PagesViewModels/DeliveryPageViewModel.cs
+++ ViewModels/PagesViewModels/DeliveryPageViewModel.cs
@@ -1,6 +1,37 @@
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
 namespace desktop.ViewModels.PagesViewModels;
 
 public partial class DeliveryPageViewModel: ViewModelBase
 {
+    [RelayCommand]
+    private async Task Save()
+    {
 
+    }
+
+    [RelayCommand]
+    private async Task Update()
+    {
+
+    }
+
+    [RelayCommand]
+    private async Task Delete()
+    {
+
+    }
+
+    [RelayCommand]
+    private async Task DriverUpdate()
+    {
+
+    }
+
+    [RelayCommand]
+    private async Task StatusUpdate()
+    {
+
+    }
 }
