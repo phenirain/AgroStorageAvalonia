697d6f475da524cb41a773e37f09e9e3f990b49e
phenirain <bigrus1998@gmail.com>
2025-01-26 16:48:20 +0300

    feat: added client page without logic

--- ViewModels/PagesViewModels/ClientPageViewModel.cs
+++ ViewModels/PagesViewModels/ClientPageViewModel.cs
@@ -1,6 +1,25 @@
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
 namespace desktop.ViewModels.PagesViewModels;
 
 public partial class ClientPageViewModel: ViewModelBase
 {
+    [RelayCommand]
+    private async Task Save()
+    {
 
+    }
+
+    [RelayCommand]
+    private async Task Update()
+    {
+
+    }
+
+    [RelayCommand]
+    private async Task Delete()
+    {
+
+    }
 }
