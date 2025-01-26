1fc5a3b9068b1bc0bed75ed6aa4be7ddb26ee6e2
phenirain <bigrus1998@gmail.com>
2025-01-26 14:58:33 +0300

    feat: added order info page without logic

--- ViewModels/PagesViewModels/Orders/OrderInfoPageViewModel.cs
+++ ViewModels/PagesViewModels/Orders/OrderInfoPageViewModel.cs
@@ -1,6 +1,13 @@
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
 namespace desktop.ViewModels.PagesViewModels.Orders;
 
 public partial class OrderInfoPageViewModel: ViewModelBase
 {
+    [RelayCommand]
+    private async Task Back()
+    {
 
+    }
 }
