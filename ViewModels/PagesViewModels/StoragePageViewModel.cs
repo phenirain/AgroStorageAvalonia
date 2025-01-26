7a14a6f80682475013e73f2d26ac25dd31f13eaa
phenirain <bigrus1998@gmail.com>
2025-01-26 16:56:35 +0300

    feat: added storage page without logic

--- ViewModels/PagesViewModels/StoragePageViewModel.cs
+++ ViewModels/PagesViewModels/StoragePageViewModel.cs
@@ -1,6 +1,26 @@
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
 namespace desktop.ViewModels.PagesViewModels;
 
 public partial class StoragePageViewModel: ViewModelBase
 {
 
+    [RelayCommand]
+    private async Task Save()
+    {
+
+    }
+
+    [RelayCommand]
+    private async Task Update()
+    {
+
+    }
+
+    [RelayCommand]
+    private async Task Delete()
+    {
+
+    }
 }
