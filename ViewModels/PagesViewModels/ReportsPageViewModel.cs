f3d6e4e1c76c977ce61894f44b7c3f3b3399280a
phenirain <bigrus1998@gmail.com>
2025-01-26 16:21:34 +0300

    feat: added reports page

--- ViewModels/PagesViewModels/ReportsPageViewModel.cs
+++ ViewModels/PagesViewModels/ReportsPageViewModel.cs
@@ -1,6 +1,26 @@
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
 namespace desktop.ViewModels.PagesViewModels;
 
 public partial class ReportsPageViewModel: ViewModelBase
 {
 
+    [RelayCommand]
+    private async Task CreateStorageRemainsReportCommand()
+    {
+
+    }
+
+    [RelayCommand]
+    private async Task CreateCompletedOrdersReportCommand()
+    {
+
+    }
+
+    [RelayCommand]
+    private async Task CreateRevenueReportCommand()
+    {
+
+    }
 }
